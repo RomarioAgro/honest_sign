d9c09e576e6e2fa70edaa6cb54859a3c2f9a2d99
Кожин Роман <resume-gl@yandex.ru>
2024-10-04 15:18:39 +0300

    получение токенов ЧЗ, полного токена и токена для разрешительного режима

--- honest_sign_get_token_OOP.py
+++ honest_sign_get_token_OOP.py
@@ -1,3 +1,4 @@
+import uuid
 from sys import exit
 import logging
 import requests
@@ -5,18 +6,16 @@ import os
 import datetime
 import win32com.client
 import json
-from typing import Any
+from typing import Any, Dict
 from decouple import config as conf_token
 import copy_env_to_script_py
 from honest_sign_list_org import InnToCode
 import socket
 import telebot
-import getpass
-
+import configparser
 
 CAPICOM_LOCAL_MACHINE_STORE = 2
 INN_BELETAG = '5902025531'
-
 
 logging.basicConfig(
     filename=os.environ.get('Temp') + '\\' + os.path.basename(__file__)[:-3] + '_' + datetime.date.today().strftime(
@@ -26,36 +25,46 @@ logging.basicConfig(
     format="%(asctime)s - %(filename)s - %(funcName)s: %(lineno)d - %(message)s",
     datefmt='%H:%M:%S')
 
+# Создание объекта конфигурации
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+def get_uuid_data():
+    """
+    функция получения uuid и data для дальнейшего получения токена ЧЗ
+    делаем запрос в ЧЗ
+    :return:
+    """
+    try:
+        url = config['crpt_prod']['url'] + '/api/v3/true-api/auth/key'
+        r = requests.get(url=url)
+    except Exception as exc:
+        logging.debug(exc)
+        send_telegram(error_text='{0}'.format(exc))
+        exit(99)
+    return r.json()
+
 
 class GetTokenHonestSign:
 
-    def __init__(self, org_inn: str = '43000000000', org_sklad: list = None) -> None:
+    def __init__(self, org_inn: str = '43000000000', org_sklad: list = None, i_uuid='', i_data='') -> None:
         """
         конструктор класса, инициализируем сразу и дата и ююид
         и серийник сертификата
         """
-        try:
-            url = 'https://markirovka.crpt.ru/api/v3/true-api/auth/key'
-            r = requests.get(url=url)
-        except Exception as exc:
-            logging.debug(exc)
-            send_telegram(error_text='{0}'.format(exc))
-            exit(99)
         if org_sklad is None:
             org_sklad = []
         self.org = ''
-        self.uuid = r.json()['uuid']
-        self.data = r.json()['data']
+        self.uuid = i_uuid
+        self.data = i_data
         self.inn = org_inn
         self.destination_folder = org_sklad
         # self.serial_sert = conf_token('serial_number', default=None)
         self.error = False
         self.signed_string = self.get_signed_string()
-
-        if not self.error:
-            self.token = self.get_token()
-        else:
-            self.token = ''
+        self.permission_mode_token = None
+        self.token = None
 
     def getSignerCertificate(self) -> Any:
         """
@@ -124,6 +133,11 @@ class GetTokenHonestSign:
         return out_data3
 
     def get_token(self):
+        """
+        получение авторизационного токена на получение приватной информации по КМ
+        короче это не тот токен что при разрешительном режиме
+        :return:
+        """
         i_dict = {
             'uuid': self.uuid,
             'data': self.signed_string
@@ -131,7 +145,7 @@ class GetTokenHonestSign:
         headers = {
             "content-type": "application/json;charset=UTF-8",
         }
-        url = 'https://markirovka.crpt.ru/api/v3/true-api/auth/simpleSignIn'
+        url = config['crpt_prod']['url'] + '/api/v3/true-api/auth/simpleSignIn'
         data = json.dumps(i_dict)
         try:
             r = requests.post(url=url, data=data, headers=headers)
@@ -148,19 +162,45 @@ class GetTokenHonestSign:
                                                                   token=i_token))
         return i_token
 
+    def get_token_permission_mode(self):
+        """
+        получение токена разрешительного режима на получение публичной информации по КМ
+        :return:
+        """
+        headers = {
+            "Content-Type": "application/json"
+        }
+        url = config['crpt_prod']['url'] + '/api/v3/true-api/auth/permissive-access'
+        data = {
+            "data": self.signed_string
+        }
+        try:
+            r = requests.post(url=url, headers=headers, data=json.dumps(data))
+        except Exception as exc:
+            logging.debug(
+                f'org={self.org} инн={self.inn} запрос токена разрешительного режима закончился ошибкой {exc}')
+        if r.status_code == 200:
+            i_token = r.json()["access_token"]
+        else:
+            i_token = ''
+        logging.debug('орг={org}, ПОЛУЧИЛИ ТОКЕН: {token}'.format(org=self.org,
+                                                                  token=i_token))
+        return i_token
 
-def make_env(i_token: str = '') -> None:
+
+def make_env(tokens, mode: str = 'w') -> None:
     """
     функция сохранения токена в файл .env
     :param i_token:
     :return:
     """
-    o_token = 'token = ' + i_token + '\n'
     o_string = 'description = file .env make in comp {0} \n'.format(os.environ['COMPUTERNAME'])
-    with open('token.env', 'w') as f_env:
+    with open('token.env', mode) as f_env:
         f_env.write(o_string)
-        f_env.write(o_token)
-        # f_env.write('\n')
+        for key, value in tokens.items():
+            # Записываем в файл каждую пару "ключ = значение"
+            f_env.write(f'{key} = {value}\n')
+
 
 def send_token_to_site(token: str = ''):
     """
@@ -178,6 +218,7 @@ def send_token_to_site(token: str = ''):
 
 def copy_env():
     pass
+
 
 def send_telegram(error_text: str = 'not error'):
     """
@@ -198,25 +239,44 @@ def send_telegram(error_text: str = 'not error'):
     my_bot.send_message(id_roman, '<b>{0}</b>'.format(my_dict), parse_mode='html')
 
 
+def make_token_dict(inn, code_sklad, auth_data) -> Dict:
+    """
+    функция получения словаря токенов, полный токен и токен разрешительного режима
+    :return: dict токены ЧЗ
+    """
+    if auth_data is None:
+        auth_data['uuid'] = uuid.uuid1()
+        auth_data['data'] = 'random string'
+    i_honest_sign = GetTokenHonestSign(org_inn=inn,
+                                       org_sklad=code_sklad,
+                                       i_uuid=auth_data['uuid'],
+                                       i_data=auth_data['data'])
+    print('пошли дальше')
+    tokens = {}
+    tokens['token_full'] = i_honest_sign.get_token()
+    tokens['token_pm'] = i_honest_sign.get_token_permission_mode()
+    return tokens
+
 
 def main():
     inn_to_code = InnToCode()
     inn_to_code.read_f_make_inn_code_sklad()
     for inn, code_sklad in inn_to_code.dict_inn_code.items():
+        auth_data = get_uuid_data()
         logging.debug('ЗАШЛИ В ИНН: ' + inn)
-        i_honest_sign = GetTokenHonestSign(org_inn=inn, org_sklad=code_sklad)
-        print('пошли дальше')
-        print(i_honest_sign.token)
-        make_env(i_token=i_honest_sign.token)
+        logging.debug(f'зашли в получение uuid и data')
+        logging.debug(f'получили uuid и data {auth_data}')
+        token_dict = make_token_dict(inn, code_sklad, auth_data)
+        make_env(token_dict, mode='w')
         if inn == INN_BELETAG:
             # отправка токена на сайт бельетажа, им тоже надо чипы проверять
-            send_token_to_site(token=i_honest_sign.token)
+            send_token_to_site(token=token_dict.get('token_full', None))
         write_path = '\\\\shoprsync\\rsync\\script_py\\'
         file_to_copy = 'token.env'
         new_name_file = '.env'
         sub_dir = 'honest_sign'
         # list_folders = os.listdir(write_path)
-        if not i_honest_sign.error:
+        if not token_dict.get('token_pm', None):
             list_folders = code_sklad
             try:
                 copy_env_to_script_py.make_subfolder(root_folder=write_path, top_folders=list_folders,
